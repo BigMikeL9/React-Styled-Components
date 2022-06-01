079b08fc5d8958e606626a2adbf18d41e9e21dfe
MikeL Kamel <mikel1832@yahoo.com>
2022-06-01 17:21:08 -0400

    Styling components using styled-components packages & CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,54 @@
 import React, { useState } from "react";
 
+// -------- 🟡 👇 Using 'styled-components' library 👇 🟡 --------
+import styled from "styled-components";
+
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+
+// -------- 🟢 👇 Using 'CSS Modules'  👇 🟢 --------
+import styles from "./CourseInput.module.css";
+
+console.log(styles);
+
+// ------------------------------------------------------------------------------------------------
+// -------- 🟡 👇 Using 'styled-components' library 👇 🟡 --------
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+    border: ${(props) => (props.invalid ? "2px solid red" : "1px solid #ccc")};
+    background-color: ${(props) =>
+      props.invalid ? "rgba(250, 128, 114, 0.5)" : "transparent"};
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border: ${(props) =>
+      props.invalid ? "2px solid red" : "1px solid #8b005d"};
+  }
+
+  &.invalid input {
+    border: 2px solid red;
+    background-color: rgba(250, 128, 114, 0.5);
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -30,11 +77,32 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      {/* Contidional Styling using Template Literals*/}
-      <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+      {/* // ------------------------------------------------------------------------------------------------
+          // -------- 🟡 👇 Using 'styled-components' library 👇 🟡 -------- */}
+      {/* 👇 Using 'styled-components' library to ADD scoped styling to '<FormControl></FormControl>' custom element, and thus it doesn't need a class 👇 */}
+
+      {/* using 'props' with 'styled-components */}
+      <FormControl invalid={!isValid}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
+
+      {/* adding classes dynamically with 'styled-components */}
+      <FormControl className={!isValid && "invalid"}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
+
+      {/* // ------------------------------------------------------------------------------------------------
+          // -------- 🟢 👇 Using 'CSS Modules'  👇 🟢 -------- */}
+      {/* 👇 Contidional Styling using Template Literals */}
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
+
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,10 +1,48 @@
+// ------------------------------------------------------------------------------------------------
+// -------- 🟡 👇 Using 'styled-components' library 👇 🟡 --------
+// import styled from "styled-components";
+
+// // console.dir(styled);
+
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media screen and (max-width: 37.5em) {
+//     margin-top: 0.75rem;
+//     width: 100%;
+//   }
+
+//   &:focus {
+//     outline: none;
+//   }
+
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
+
+// ------------------------------------------------------------------------------------------------
+// -------- 🟢 👇 Using 'CSS Modules'  👇 🟢 --------
+
 import React from "react";
 
-import "./Button.css";
+// importing 'CSS Module' file
+import styles from "./Button.module.scss";
+
+// console.log(styles);
 
 const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
